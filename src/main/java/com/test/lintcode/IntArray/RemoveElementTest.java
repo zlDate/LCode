6a15bb83749da32c1e9a7e530053777949bf9b22
zengle1992 <18634342425@163.com>
2017-02-25 22:41:31 +0800

    数组删除元素

---
+++ src/main/java/com/test/lintcode/IntArray/RemoveElementTest.java
@@ -0,0 +1,26 @@
+package com.test.lintcode.IntArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by le on 2017/2/25.
+ * 给定一个数组和一个值，在原地删除与值相同的数字，返回新数组的长度。
+ * 元素的顺序可以改变，并且对新的数组不会有影响。
+ * 给出一个数组 [0,4,4,0,0,2,4,4]，和值 4
+ * 返回 4 并且4个元素的新数组为[0,0,0,2]
+ */
+public class RemoveElementTest {
+
+    public int removeElement(int[] A, int elem) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<A.length;i++){
+            if(A[i]!=elem) list.add(A[i]);
+        }
+        for(int i=0;i<list.size();i++){
+            A[i] = list.get(i);
+        }
+        return list.size();
+    }
+
+}
