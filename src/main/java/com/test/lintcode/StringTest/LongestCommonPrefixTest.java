24ca53b912ed9144a007dfb06d219942eff17d0d
zengle1992 <18634342425@163.com>
2017-03-15 21:34:07 +0800

    最长公共前缀增加一种方法

--- src/main/java/com/test/lintcode/StringTest/LongestCommonPrefixTest.java
+++ src/main/java/com/test/lintcode/StringTest/LongestCommonPrefixTest.java
@@ -9,7 +9,13 @@ package com.test.lintcode.StringTest;
  */
 public class LongestCommonPrefixTest {
 
-    public String longestCommonPrefix(String[] strs) {
+    public static void main(String[] args) {
+        String[] strs = new String[]{"ABCDEFG","ABCEFG","ABCEFA"};
+        System.out.println(longestCommonPrefix2(strs));
+    }
+
+    //1691  1808  1853
+    public static String longestCommonPrefix(String[] strs) {
         if(strs.length==0) return "";
         //找出长度最短的字符串   有一样短的随便选一个
         String shortest=strs[0];
@@ -28,6 +34,41 @@ public class LongestCommonPrefixTest {
             index=0;
         }
         return "";
+    }
+
+    public static String longestCommonPrefix2(String[] strs) {
+        if(strs.length==0) return "";
+        if(strs.length==1) return strs[0];
+        //找出长度最短的字符串   有一样短的随便选一个
+        String shortest=strs[0];
+        for(String str:strs){
+            if(shortest.length()>str.length())shortest = str;
+        }
+
+        int length = 0;
+        boolean flag=true;
+        for(int i=0;i<shortest.length();i++){
+            int index = 0;
+            char tmp = shortest.charAt(i);
+
+            for(int j=0;j<strs.length;j++){
+                if(tmp!=strs[j].charAt(i)){
+                    break;
+                }else index++;
+
+            }
+
+            if(index!=strs.length) {
+                length= i;
+                flag=false;
+                break;
+            }
+
+        }
+
+        String result = flag?shortest.substring(0,length+1):shortest.substring(0,length);
+
+        return result;
     }
 
 }
