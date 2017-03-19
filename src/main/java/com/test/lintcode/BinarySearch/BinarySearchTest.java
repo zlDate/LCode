f81517c9b9b94defcd9363fdba37893465dda1e5
zengle1992 <18634342425@163.com>
2017-03-19 22:02:35 +0800

    二分查找添加时间复杂度注释

--- src/main/java/com/test/lintcode/BinarySearch/BinarySearchTest.java
+++ src/main/java/com/test/lintcode/BinarySearch/BinarySearchTest.java
@@ -7,6 +7,11 @@ package com.test.lintcode.BinarySearch;
  * 用O(logn)的时间查找到target第一次出现的下标（从0开始），
  * 如果target不存在于数组中，返回-1。
  * 在数组 [1, 2, 3, 3, 4, 5, 10] 中二分查找3，返回2
+ *
+ * 二分查找中 时间复杂度计算可以堪称while循环次数
+ * 第一次循环查找区间为n第二次为n/2 第三次为 n/4 第k次为 n/2^k
+ * 最后 n/2^k>=1所以 2^k=n  k=log(n)（以2为底）
+ *
  */
 public class BinarySearchTest {
 
