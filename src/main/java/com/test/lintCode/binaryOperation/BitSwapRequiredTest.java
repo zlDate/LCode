f56654bd410189d54f91b0c4ace7e94ca6349127
zengle1992 <18634342425@163.com>
2017-04-04 16:28:36 +0800

    181 将整数A转换成B

--- src/main/java/com/test/lintCode/binaryOperation/BitSwapRequiredTest.java
+++ src/main/java/com/test/lintCode/binaryOperation/BitSwapRequiredTest.java
@@ -7,27 +7,41 @@ package com.test.lintCode.binaryOperation;
  * 如把31转换为14，需要改变2个bit位。
  * (31)10=(11111)2
  * (14)10=(01110)2
+ * 异或运算 计算结果中有多少1就是要改变的bit位数
  */
 public class BitSwapRequiredTest{
 
     public static void main(String[] args) {
 
-        System.out.println(bitSwapRequired(1,2));
+//        bitSwapRequired2(31,14);
+
+        String[] strs = "1000".split("1");
+
+        System.out.println("1234".charAt(4));
+
+
+
     }
 
 
-
-    public static String bitSwapRequired(int a, int b) {
-        String x = "a";
-        try{
-            x += "b";
-            return x;
-        }catch (Exception e){
-            x += "d";
-            return x;
-        }finally {
-            x+= "c";
+    private static int bitSwapRequired(int a, int b) {
+        int count = 0;
+        for(int i=a^b;i!=0;i>>>=1){
+            count += i&1;
         }
+        return count;
+    }
+
+    private static int bitSwapRequired2(int a, int b) {
+        int num = a^b;
+        String x = Integer.toBinaryString(num);
+        int count = 0;
+        for(int i=0;i<x.length();i++){
+            if(x.charAt(i)=='1'){
+                count++;
+            }
+        }
+        return count;
     }
 
 }
